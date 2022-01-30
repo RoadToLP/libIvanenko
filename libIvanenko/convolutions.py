c7b0795c3f1d4f057ae3f6f75c2937677bde256c
RoadToLP <ilya.titoff2013@yandex.ru>
2022-01-30 15:31:49 +0300

    фикс сверток

--- libIvanenko/convolutions.py
+++ libIvanenko/convolutions.py
@@ -20,9 +20,9 @@ def rconvo2dphys(x, h):
 
 def lconvo2dphys(x, h):
     y = []
-    for m in range(len(x[0]) + len(h) - 1):
+    for m in range(len(x) + len(h) - 1):
         part = []
-        for n in range(len(x) + len(h[0]) - 1):
+        for n in range(len(x[0]) + len(h[0]) - 1):
             part.append(sum([x[l][k] * linearExtract2d(h, m-l, n-k) for k in range(len(x[0])) for l in range(len(x))]))
         y.append(part)
     return y
@@ -38,9 +38,9 @@ def rconvo2dalg(x, h):
 
 def lconvo2dalg(x, h):
     y = []
-    for n in range(len(x[0]) + len(h[0]) - 1):
+    for m in range(len(x) + len(h) - 1):
         part = []
-        for m in range(len(x) + len(h) - 1):
+        for n in range(len(x[0]) + len(h[0]) - 1):
             part.append(sum([x[k][l] * linearExtract2d(h, m-k, n-l) for k in range(len(x)) for l in range(len(x[0]))]))
         y.append(part)
     return y
